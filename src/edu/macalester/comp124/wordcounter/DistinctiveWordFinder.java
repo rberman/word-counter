00fa3a9314c8ac721bdbd4e5b11789e8250937b9
rberman <rberman@macalester.edu>
2014-03-07 11:29:17 -0600

    Distinctive Word Finder class edited.

--- src/edu/macalester/comp124/wordcounter/DistinctiveWordFinder.java
+++ src/edu/macalester/comp124/wordcounter/DistinctiveWordFinder.java
@@ -9,67 +9,66 @@ import java.util.Arrays;
 /**
  * Interesting word finder.
  * To use this class, first call countWords(), then call findDistinctive.
- * 
+ *
  * Your tasks are:
  * 1. Complete the countWordsInOneFile method (you only need to add three lines of code).
  * 2. Complete the findInterestingWordsOneWay method (this should be done in 3 stages).
- * 
+ *
  * @author shilad
  *
  */
 public class DistinctiveWordFinder {
-	// Word counts from each file.
-	private AllWordsCounter primaryCounts;
-	private AllWordsCounter secondaryCounts;
-	
-	/**
-	 * Constructs a new word counter.
-	 */
-	public DistinctiveWordFinder() {
+    // Word counts from each file.
+    private AllWordsCounter primaryCounts;
+    private AllWordsCounter secondaryCounts;
+
+    /**
+     * Constructs a new word counter.
+     */
+    public DistinctiveWordFinder() {
         primaryCounts = new AllWordsCounter();
         secondaryCounts = new AllWordsCounter();
-	}
-	
-	/**
-	 * Counts the words in two files.
-	 * @param primaryFile
-	 * @param secondaryFile
-	 * @throws java.io.IOException
-	 */
-	public void countWords(File primaryFile, File secondaryFile) throws IOException {
-		countWordsInOneFile(primaryFile, primaryCounts);
-		countWordsInOneFile(secondaryFile, secondaryCounts);
-	}
+    }
 
-	/**
-	 * Counts the words in a single file.  The counts should be tallied
-	 * in the passed-in counts object.
-	 *
-	 * @param file1
-	 * @param counter
-	 * @throws java.io.IOException
-	 */
-	private void countWordsInOneFile(File file1, AllWordsCounter counter) throws IOException {
-		BufferedReader r = new BufferedReader(new FileReader(file1));
-		while (true) {
-			String line = r.readLine();
-			if (line == null) {
-				break;
-			}
+    /**
+     * Counts the words in two files.
+     * @param primaryFile
+     * @param secondaryFile
+     * @throws java.io.IOException
+     */
+    public void countWords(File primaryFile, File secondaryFile) throws IOException {
+        countWordsInOneFile(primaryFile, primaryCounts);
+        countWordsInOneFile(secondaryFile, secondaryCounts);
+    }
+
+    /**
+     * Counts the words in a single file.  The counts should be tallied
+     * in the passed-in counts object.
+     *
+     * @param file1
+     * @param counter
+     * @throws java.io.IOException
+     */
+    private void countWordsInOneFile(File file1, AllWordsCounter counter) throws IOException {
+        BufferedReader r = new BufferedReader(new FileReader(file1));
+        while (true) {
+            String line = r.readLine();
+            if (line == null) {
+                break;
+            }
             for (String w : splitLine(line)) {
                 counter.count(w);
             }
-		}
-	}
-	
-	/**
-	 * Find and display words in the primary word counter that are interesting.
-	 * Use System.out.println() to print out information about the interesting
-	 * words.
-	 */
-	private void findDistinctive() {
-        // TODO: initialize to words in primary counter
-        String words[] = null;
+        }
+    }
+
+    /**
+     * Find and display words in the primary word counter that are interesting.
+     * Use System.out.println() to print out information about the interesting
+     * words.
+     */
+    private void findDistinctive() {
+        String words[] = primaryCounts.getAllWords();
 
         // This array will be used to sort the word scores;
         WordScore scores[] = new WordScore[words.length];
@@ -77,10 +76,9 @@ public class DistinctiveWordFinder {
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
 
-            // TODO: correctly get the primary and secondary counts from
             // the primaryCounts and secondaryCounts instance variables.
-            int primary = 0;
-            int secondary = 0;
+            int primary = primaryCounts.getCount(word);
+            int secondary = secondaryCounts.getCount(word);
 
             // Calculate the interestingness score and prepare it in the output queue
             double score = getDistinctiveScore(primary, secondary);
@@ -95,7 +93,7 @@ public class DistinctiveWordFinder {
             int c2 = secondaryCounts.getCount(word);
             System.out.println("word: " + word + ", primary=" + c1 + ", secondary=" + c2);
         }
-	}
+    }
 
     /**
      * Returns a "score" indicating how interesting a word is.
@@ -104,28 +102,26 @@ public class DistinctiveWordFinder {
      * @return
      */
     private double getDistinctiveScore(int primaryCount, int secondaryCount) {
-        // TODO: return something reasonable.
-        // A higher value means more "distinctive".
-        return 0.0;
+        return primaryCount / (primaryCount + secondaryCount + 3);
     }
 
-	/**
-	 * Splits a line into words.
-	 * @param line
-	 * @return An array containing the words.
-	 */
-	private String [] splitLine(String line) {
-		return line.split("[^a-zA-Z0-9']+");
-	}
-	
-	public static void main(String args[]) throws IOException {
-        File f1 = new File("dat/palin.txt");
-        File f2 = new File("dat/biden.txt");
+    /**
+     * Splits a line into words.
+     * @param line
+     * @return An array containing the words.
+     */
+    private String [] splitLine(String line) {
+        return line.split("[^a-zA-Z0-9]+");
+    }
 
-		DistinctiveWordFinder finder = new DistinctiveWordFinder();
-		finder.countWords(f1, f2);
-		finder.findDistinctive();
-	}
+    public static void main(String args[]) throws IOException {
+        File f1 = new File("/Users/rberman/IdeaProjects/word-counter/dat/palin.txt");
+        File f2 = new File("/Users/rberman/IdeaProjects/word-counter/dat/biden.txt");
+
+        DistinctiveWordFinder finder = new DistinctiveWordFinder();
+        finder.countWords(f1, f2);
+        finder.findDistinctive();
+    }
 
     /**
      * Helper class used for sorting word scores.
